f39dcc74decbc92f821c59a5dcad452b529aa093
shalevwiseman <shalevwisema@campus.technion.ac.il>
2023-12-02 16:07:05 +0200

    CRUD

---
+++ src/model/user.js
@@ -0,0 +1,70 @@
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    profilePicture: {
+        type: String,
+        default: ''
+    },
+    bio: {
+        type: String,
+        default: ''
+    },
+    followers: {
+        type: [mongoose.Schema.Types.ObjectId],
+        default: []
+    },
+    following: {
+        type: [mongoose.Schema.Types.ObjectId],
+        default: []
+    },
+    posts: {
+        type: [mongoose.Schema.Types.ObjectId],
+        default: []
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+    
+});
+
+// create a model by mogoose
+const userModel = mongoose.model('User', userSchema);
+
+// initialing the schema for validation parameters
+const joiUserSchema = Joi.object({
+    username: Joi.string().min(3).max(30).required(),
+    email: Joi.string().min(5).max(50).required().email(),
+    password: Joi.string().min(8).required(),
+    profilePicture: Joi.string(),
+    bio: Joi.string(),
+    followers: Joi.array(),
+    following: Joi.array(),
+    posts: Joi.array(),
+    isAdmin: Joi.boolean()
+});
+
+// validation using Joi & finally return the result of validation
+export const validateUser = (user) => joiUserSchema.validate(user);
+
+export default userModel;
---
+++ src/routes/user.js
@@ -0,0 +1,141 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import User, {validateUser} from '../model/user.js';
+
+const router = express.Router();
+
+// Get all users
+router.get('/', async (req, res) => {
+    const users = await User.find().sort('username');
+    return res.json({
+        success: true,
+        data: users,
+        message: 'Successfully fetched all users!',
+        })
+});
+
+// Get a single user
+router.get('/:id', async (req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).json({
+            success: false,
+            data: [],
+            message: 'Invalid user id',
+        })
+    }
+    // search for the user in the database
+    const user = await User.findById(req.params.id);
+    // check if user not found then return 404
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            data: [],
+            message: 'User not found',
+        })
+    }
+    // return the user object if found
+    return res.json({
+        success: true,
+        data: user,
+        message: 'Successfully fetched the user!',
+        })
+});
+
+// update an existing user
+router.put('/:id', async (req, res) => {
+    // validate the request body first
+    const {error} = validateUser(req.body);
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            data: [],
+            message: error?.details[0]?.message,
+        })
+    }
+    // find the user by id and update it with the request body
+    const user = await User.findByIdAndUpdate(req.params.id, {
+        username: req.body.username,
+        email: req?.body?.email,
+        password: req.body.password,
+        profilePicture: req.body.profilePicture,
+        bio: req.body.bio,
+        followers: req.body.followers,
+        following: req.body.following,
+        posts: req.body.posts,
+        isAdmin: req.body.isAdmin
+    }, {new: true});
+    // check if user not found then return 404
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            data: [],
+            message: 'User not found',
+        })
+    }
+    // return the updated user object
+    return res.json({
+        success: true,
+        data: user,
+        message: 'Successfully updated the user!',
+        })
+});
+
+// delete an existing user
+router.delete('/:id', async (req, res) => {
+    const userId = req.params.id;
+    // find the user by id and delete it
+    const deletedUser = await User.findOneAndDelete({ _id: userId });
+    // check if user not found then return 404
+    if (!deletedUser) {
+        return res.status(404).json({
+            success: false,
+            data: [],
+            message: 'User not found',
+        })
+    }
+    // return the deleted user object
+    return res.json({
+        success: true,
+        data: deletedUser,
+        message: 'Successfully deleted the user!',
+        })
+});
+
+
+
+// insert a new user
+router.post('/', async (req, res) => {
+    // validate the request body first
+    const {error} = validateUser(req.body);
+    if (error) { 
+        return res.status(400).json({
+        success: false,
+        data: [],
+        message: error?.details[0]?.message,
+      })
+    }
+
+    // check if the user already exists
+    let user = new User({
+        username: req.body.username,
+        email: req?.body?.email,
+        password: req.body.password,
+        profilePicture: req.body.profilePicture,
+        bio: req.body.bio,
+        followers: req.body.followers,
+        following: req.body.following,
+        posts: req.body.posts,
+        isAdmin: req.body.isAdmin
+    });
+
+
+    user = await user.save();
+
+    return res.json({
+        success: true,
+        data: user,
+        message: 'New todo adding successful!',
+      })
+});
+
+export default router;
