d382fe08b12be7cb60614598f0af9b349eabb4c9
shalevwiseman <shalevwisema@campus.technion.ac.il>
2023-12-14 16:03:21 +0200

    add posts microService

---
+++ server/src/routes/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { validateUser, userModel } = require('../model/user.js');
+const e = require('express');
+
+
+
+const router = express.Router();
+
+
+module.exports = router;
